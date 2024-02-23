6101b2e8af82c0b8b81b0c96dffd24e678425bcc
Sivani <sivanilr03@gmail.com>
2024-02-23 16:04:29 +0530

    signout functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_app/auth/auth.dart';
 import 'package:social_media_app/firebase_options.dart';
 import 'package:social_media_app/pages/login.dart';
 import 'package:social_media_app/pages/start.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StartPage(),
+      home: AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
     );
--- lib/pages/mainscreen.dart
+++ lib/pages/mainscreen.dart
@@ -1,12 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
+  void logoutUser()
+  {
+    FirebaseAuth.instance.signOut();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: [
+          IconButton(
+            onPressed: logoutUser, 
+            icon: Icon(Icons.logout_rounded),),
+          
+        ],
+      ),
     );
   }
 }
