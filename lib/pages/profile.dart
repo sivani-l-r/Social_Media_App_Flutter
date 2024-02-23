0c0d4920dbcd231f0b8b5c1636be47abe7738c63
Sivani <sivanilr03@gmail.com>
2024-02-23 19:49:10 +0530

    Users & Profile Page Functionality added

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,8 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:social_media_app/components/drawer.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  Future<DocumentSnapshot<Map<String,dynamic>>> getUserData() async
+  {
+      return await FirebaseFirestore.instance
+      .collection("Users")
+      .doc(currentUser!.email)
+      .get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +27,84 @@ class ProfilePage extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: DrawerWidget(),
+      body: FutureBuilder<DocumentSnapshot<Map<String,dynamic>>>(
+        future: getUserData(),
+        builder:(context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting)
+          {
+            return Center(
+            child: Lottie.network('https://lottie.host/7771e78d-39dc-40f9-b271-9c75d264d0e8/bgSKwTFkza.json')
+            );
+          }
+          else if(snapshot.hasError)
+          {
+            return Text("Error: ${snapshot.error}");
+          }
+          else if(snapshot.hasData)
+          {
+            Map<String, dynamic>? user = snapshot.data!.data();
+
+            return ProfilePageDetails(email: user!['email'], username: user['username']);
+          }
+          else
+          {
+            return Text('No Data');
+          }
+          
+        },
+      ),
+    );
+  }
+}
+
+class ProfilePageDetails extends StatelessWidget {
+  final String email;
+  final String username;
+
+  const ProfilePageDetails({
+    Key? key,
+    required this.email,
+    required this.username,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              title: Text(
+                'Username',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              subtitle: Text(
+                username,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            Divider(), // Adding a divider for visual separation
+            ListTile(
+              title: Text(
+                'Email',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              subtitle: Text(
+                email,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/users.dart
+++ lib/pages/users.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:social_media_app/components/drawer.dart';
 
 class UsersPage extends StatelessWidget {
@@ -14,6 +16,44 @@ class UsersPage extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: DrawerWidget(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore
+        .instance
+        .collection("Users")
+        .snapshots(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting)
+          {
+            return Center(
+            child: Lottie.network('https://lottie.host/7771e78d-39dc-40f9-b271-9c75d264d0e8/bgSKwTFkza.json')
+            );
+          }
+          else if(snapshot.hasError)
+          {
+            return Text("Error: ${snapshot.error}");
+          }
+          else if(snapshot.hasData)
+          {
+            final users = snapshot.data!.docs;
+
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+
+                return ListTile(
+                  title: Text(user['username']),
+                  subtitle: Text(user['email']),
+                );
+              },
+            );
+          }
+          else
+          {
+            return Text('No Data');
+          }
+        },
+      ),
     );
   }
 }
