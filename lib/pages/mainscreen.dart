f3cf8961f2be27fa706912b7d221f54cceab1e8e
Sivani <sivanilr03@gmail.com>
2024-02-24 09:38:26 +0530

    display posts functionality

--- lib/pages/mainscreen.dart
+++ lib/pages/mainscreen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:social_media_app/components/drawer.dart';
+import 'package:intl/intl.dart';
 import 'package:social_media_app/database/firestore.dart';
 
 class MainPage extends StatelessWidget {
@@ -60,10 +63,99 @@ class MainPage extends StatelessWidget {
           ),
         ),
 
+        StreamBuilder(
+          stream: database.getPostStream(), 
+          builder:(context, snapshot) {
+                if(snapshot.connectionState == ConnectionState.waiting)
+                  {
+                    return Center(
+                    child: Lottie.network('https://lottie.host/7771e78d-39dc-40f9-b271-9c75d264d0e8/bgSKwTFkza.json')
+                    );
+                  }
+
+                final posts = snapshot.data!.docs;
+
+                if(snapshot.data == null || posts.isEmpty)
+                {
+                  return const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(25),
+                      child: Text("No posts.. share somethingg!"),
+                    ),
+                  );
+                }
+
+                return Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, 
+                          crossAxisSpacing: 8.0,
+                          mainAxisSpacing: 8.0,
+                        ),
+                        itemCount: posts.length,
+                        itemBuilder: (context, index) {
+                          final post = posts[index];
+
+                          String message = post['PostMessage'];
+                          String userEmail = post['UserEmail'];
+                          Timestamp timestamp = post['TimeStamp'];
+                          String formattedDate = DateFormat('yyyy-MM-dd').format(timestamp.toDate());
+
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              color:  Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+                              elevation: 6,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      message,
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).colorScheme.primary,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      "By: $userEmail",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+                                        
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "Posted at: $formattedDate",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );                
+                  
+          },)
+
         ],
       ),
 
     );
   }
 }
+
 
